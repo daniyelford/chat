fe755acdb03b61b072b2fedf0abc0530a1e4afe5
daniyelford <29danialfrd69@gmail.com>
2025-08-07 15:50:42 +0330

    ok:)

---
+++ ci3/application/config/token/api_token_c0a9c77451a629855ec4463fbd6eeb9de630c3bb.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+const API_TOKEN = 'd56ce4c95768fca590c974ada381149329fcae114e497b2fa3d4f5adc85d863b';
+const API_TOKEN_TIME = '1754569153';
