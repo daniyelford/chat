85a89a5e82492a07630cd4300a364b8df94f49d7
daniyelford <29danialfrd69@gmail.com>
2025-08-30 08:18:50 +0330

    ok:)

---
+++ ci3/application/config/token/api_keys_1bce2b73a3a20fc5c10261cb96078f3c5d31c4c7.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+const API_KEY = '4d2d877b0d4c237a7996f33bbfcdf963';
+const API_KEY_BACK = '9bf8ef3f7ca8e00ed50a3d3e4ffaa45f';
