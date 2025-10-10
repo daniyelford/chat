a44ea2af655ff0510b3686cfde538af24b2de433
daniyal frd <76838824+daniyelford@users.noreply.github.com>
2025-10-10 22:47:05 +0330

    ok:)

--- vue3/src/components/dashboard/pagesContent/PlacesList.vue
+++ vue3/src/components/dashboard/pagesContent/PlacesList.vue
@@ -134,7 +134,7 @@
       has_more: placeStore.hasMorePlaces,
     }
   })
-  polling(() => placeStore.fetchLatestNewsRaw({limit:placeStore?.allPlaces?.length>0?placeStore?.allPlaces?.length:10, offset:0,category_id: selectedCategoryId.value}), {
+  polling(() => placeStore.fetchPlacesPaginated({limit:placeStore?.allPlaces?.length>0?placeStore?.allPlaces?.length:10, offset:0,category_id: selectedCategoryId.value}), {
     intervalMs: 6000,
     isDifferent: (oldData, newData) => {
       if (!Array.isArray(oldData) || !Array.isArray(newData)) return true
